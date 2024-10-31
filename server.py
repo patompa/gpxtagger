8cb6f40689b4e32101607062fb061ef2f7c8509d
Thomas Sandholm <tom@patompa.com>
2024-10-30 21:25:44 -0700

    added course selection

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, send_file
+import json
+import glob
 
 app = Flask(__name__)
 
@@ -10,6 +12,13 @@ def index():
 @app.route("/map/<map>")
 def get_map(map):
   return send_file(f'maps/{map}.png', mimetype='image/png')
+
+@app.route("/list")
+def get_list():
+  courses = []
+  for fname in glob.glob("maps/*.json"):
+      courses.append(fname.split("/")[1].split(".")[0])
+  return json.dumps({"courses": courses})
 
 @app.route("/coord/<map>")
 def get_coord(map):
